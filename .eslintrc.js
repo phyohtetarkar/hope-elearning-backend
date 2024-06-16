b7330566fac9bcd97aa5abe4650cf47235999e33
Phyo Htet Arkar <phyohtetarkar@gmail.com>
2024-06-16 14:48:11 +0630

    update ts config

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,8 +20,8 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-unused-vars': 'off',
-    'prettier/prettier': 1,
+    'prettier/prettier': 'warn',
   },
 };
