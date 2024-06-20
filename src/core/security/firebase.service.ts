d1d49b3ec5d5fa90c80f431ab0906ca3c0eb9cc9
Phyo Htet Arkar <phyohtetarkar@gmail.com>
2024-06-20 20:55:20 +0630

    update readme

--- src/core/security/firebase.service.ts
+++ src/core/security/firebase.service.ts
@@ -1,16 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { initializeApp, cert } from 'firebase-admin/app';
+import { cert, initializeApp } from 'firebase-admin/app';
 import { Auth, getAuth } from 'firebase-admin/auth';
 
 @Injectable()
 export class FirebaseService {
-  private apiKey: string;
   private auth: Auth;
 
   constructor(configService: ConfigService) {
-    this.apiKey = configService.getOrThrow<string>('FIREBASE_API_KEY');
-
     const serviceAccount = configService.getOrThrow<string>(
       'FIREBASE_SERVICE_ACCOUNT',
     );
